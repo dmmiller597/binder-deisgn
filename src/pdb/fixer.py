b8193347ae78612b8ccdf06f41d998366cb79966
jakublala <jakublala@gmail.com>
2024-10-07 12:53:33 +0100

    fixer.py working

--- src/pdb/fixer.py
+++ src/pdb/fixer.py
@@ -2,69 +2,56 @@ from Bio.PDB import PDBParser, PDBIO
 from Bio.PDB.Chain import Chain
 
 
-def fixer(pdb_path):
-    print('Splitting chains...')
+def fixer(pdb_path, design_name):
+    print('Splitting chains...')        
     # SEE https://stackoverflow.com/questions/74735845/splitting-and-renaming-protein-chain-with-biopythons-biopdb
-    structure = PDBParser().get_structure('dimer1', pdb_path)
-    # get residue id where to break
-    last_id = 0
-    res_to_chain_B = []
-    res_to_chain_C = []
-    chainB = False
-    chainC = False
+    structure = PDBParser().get_structure(design_name, pdb_path)
+    # get residue ids where to break
+    all_ids = []
     for model in structure:
         for chain in model:
             for residue in chain:
-                if chainC:
-                    res_to_chain_C.append(residue)
-                    continue
-                elif chainB:
-                    res_to_chain_B.append(residue)
-                    if abs(residue.id[1] - last_id) > 2:
-                        chainC = True
-                        res_to_chain_C.append(residue)
-                    continue
-                elif abs(residue.id[1] - last_id) > 2:
-                    chainB = True
-                    res_to_chain_B.append(residue)
-                last_id = residue.id[1]
+                all_ids.append(residue.id[1])
+    # find the indices of the residues where to break
+    indices_to_break = []
+    for i in range(len(all_ids) - 1):
+        if abs(all_ids[i] - all_ids[i + 1]) > 2:
+            indices_to_break.append(i)
 
-    ### SEE https://stackoverflow.com/questions/25884758/deleteing-residue-from-pdb-using-biopython-library
-    for model in structure:
-        for chain in model:
-            for res in res_to_chain_B:
-                if res.get_id() in chain.child_dict:
-                    chain.detach_child(res.get_id())
-            for res in res_to_chain_C:
-                if res.get_id() in chain.child_dict:
-                    chain.detach_child(res.get_id())
+    chains = {'A': [], 'B': [], 'C': []}
 
-    ### SEE https://stackoverflow.com/questions/33364370/how-to-add-chain-id-in-pdb
-    my_chain_B = Chain("B")
-    my_chain_C = Chain("C")
-    model.add(my_chain_B)
-    model.add(my_chain_C)
-    for index, res in enumerate(res_to_chain_B):
-        # reindex
-        res.id = (' ', index+1, ' ')
-        my_chain_B.add(res)
-    for index, res in enumerate(res_to_chain_C):
-        # reindex
-        res.id = (' ', index+1, ' ')
-        my_chain_C.add(res)
-
-    # Create a new structure to avoid PDBConstructionException
     from Bio.PDB import Structure, Model
-    new_structure = Structure.Structure('new_dimer1')
+    new_structure = Structure.Structure(design_name)
     new_model = Model.Model(0)
     new_structure.add(new_model)
-    new_model.add(my_chain_B)
-    new_model.add(my_chain_C)
+
+    for model in structure:
+        for chain in model:
+            # get residues 0 to first break
+            residues = [i for i in chain.get_residues()]
+
+            # first chain
+            chains['A'] = residues[0:indices_to_break[0]+1]
+            # second chain
+            chains['B'] = residues[indices_to_break[0]+1:indices_to_break[1]+1]
+            # third chain
+            chains['C'] = residues[indices_to_break[1]+1:]
+            
+    # add these to a new structure
+    for chain_id, residues in chains.items():
+        i = 1
+        new_chain = Chain(chain_id)
+        for residue in residues:
+            residue.id = (residue.id[0], i, residue.id[2])
+            new_chain.add(residue)
+            i += 1
+        new_model.add(new_chain)
 
     io = PDBIO()
     io.set_structure(new_structure)
-    io.save('../data/designs/dimer1/original.pdb')
+    io.save(f'/home/jakub/phd/binder-design/data/designs/{design_name}/fixed.pdb')
 
 
 if __name__ == "__main__":
-    fixer("/home/jakub/phd/binder-design/data/stefano/dimer/CARTCD20__best_123.pdb")
+    fixer("/home/jakub/phd/binder-design/data/designs/dimer1/original.pdb", 'dimer1')
+    fixer("/home/jakub/phd/binder-design/data/designs/dimer5/original.pdb", 'dimer5')
