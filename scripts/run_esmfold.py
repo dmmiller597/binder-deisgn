9f5d3fdeb21e1d3f831c17c59400448849e7d981
jakublala <jakublala@gmail.com>
2024-10-08 10:30:10 +0100

    new final designs for simple

--- scripts/run_esmfold.py
+++ scripts/run_esmfold.py
@@ -5,6 +5,9 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from src.esmfold import EsmFoldv1
 
+from Bio.SeqIO.FastaIO import SimpleFastaParser
+import torch
+
 if __name__ == "__main__":
     device = 'cuda'
 
@@ -14,76 +17,93 @@ if __name__ == "__main__":
         memory_light=True,
     )
 
-    filepath = "../outputs/dimer5/SolubleMPNN/dimer5-solMPNN.fasta"
+    design = 'simpleAA'
 
-    # iterate through and get all sequences
-    from Bio.SeqIO.FastaIO import SimpleFastaParser
+    for design in ['simpleAA', 'simpleBB', 'simpleAB']:
 
-    sequences = []
-    with open(filepath, "r") as file:
-        for title, sequence in SimpleFastaParser(file):
-            sequences.append(sequence)
+        for j in range(0, 10):
+
+            filepath = f"outputs/{design}/SolubleMPNN/seqs/_{j}.fa"
 
 
-    for i, sequence in enumerate(sequences):
-        print(f"Running sequence {i} / {len(sequences)}: {sequence}")
-
-        # split by :
-        sequences = [sequence.split(":")[0] for sequence in sequences]
-
-        CD20_A = sequences[0]
-        CD20_B = sequences[1]
-        binder = sequences[2]
-
-        # CD20_A = "FMRESKTLGAVQIMNGLFHIALGGLLMIPAGIYAPICVTVWYPLWGGIMYIISGSLLAATEKNSRKCLVKGKMIMNSLSLFAAISGMILSIMDILNIKISHFLKMESLNFIRAHTPYINIYNCEPANPSEKNSPSTQYCYSIQSLFLGILSVMLIFAFFQELVIAGIVE"
-
-        # CD20_B = "FMRESKTLGAVQIMNGLFHIALGGLLMIPAGIYAPICVTVWYPLWGGIMYIISGSLLAATEKNSRKCLVKGKMIMNSLSLFAAISGMILSIMDILNIKISHFLKMESLNFIRAHTPYINIYNCEPANPSEKNSPSTQYCYSIQSLFLGILSVMLIFAFFQELVIAGIVE"
-        
-
-        # # with open("data/test.fasta", "r") as file:
-        # binder = 'FLIFLHHSNSYLYNDAYHNRMMQKPDDEREGDPEYALYAAQRNAHMGMMYWMYEWHNWLGFKATPNEYPGLTEVVPELTR'
-        
-        # # strip the last part of the sequence separated by :
-        # binder = binder.split(":")[0]
-
-        multimer_gap = 256
-
-        linker_sequence = "G" * 100 # need to make at least 100 so that it can circle all the way back and around
-
-        # they also suggest changing the order of the sequences
-        sequence = CD20_A + linker_sequence + CD20_B + linker_sequence + binder
-
-        multimer_gap = 256
-
-        residue_indices = list(range(1, len(sequence) + 1))
+            sequences = []
+            with open(filepath, "r") as file:
+                for title, sequence in SimpleFastaParser(file):
+                    sequences.append(sequence)
 
 
-        residue_indices[len(CD20_A) + len(linker_sequence):] = [
-            i + multimer_gap for i in residue_indices[len(CD20_A) + len(linker_sequence):]
-        ]
+            for i, sequence in enumerate(sequences):
+                print(f"Running sequence {i} / {len(sequences)}: {sequence}")
 
-        residue_indices[len(CD20_A) + len(linker_sequence) + len(CD20_B) + len(linker_sequence):] = [
-            i + multimer_gap for i in residue_indices[len(CD20_A) + len(linker_sequence) + len(CD20_B) + len(linker_sequence):]
-        ]
+                # split by :
+                sequences = [sequence.split(":")[0] for sequence in sequences]
+
+                # CD20_A = sequences[0]
+                # CD20_B = sequences[1]
+                binder = sequences[0]
+
+                CD20_A = "FMRESKTLGAVQIMNGLFHIALGGLLMIPAGIYAPICVTVWYPLWGGIMYIISGSLLAATEKNSRKCLVKGKMIMNSLSLFAAISGMILSIMDILNIKISHFLKMESLNFIRAHTPYINIYNCEPANPSEKNSPSTQYCYSIQSLFLGILSVMLIFAFFQELVIAGIVE"
+
+                CD20_B = "FMRESKTLGAVQIMNGLFHIALGGLLMIPAGIYAPICVTVWYPLWGGIMYIISGSLLAATEKNSRKCLVKGKMIMNSLSLFAAISGMILSIMDILNIKISHFLKMESLNFIRAHTPYINIYNCEPANPSEKNSPSTQYCYSIQSLFLGILSVMLIFAFFQELVIAGIVE"
+                
+
+                # # with open("data/test.fasta", "r") as file:
+                # binder = 'FLIFLHHSNSYLYNDAYHNRMMQKPDDEREGDPEYALYAAQRNAHMGMMYWMYEWHNWLGFKATPNEYPGLTEVVPELTR'
+                
+                # # strip the last part of the sequence separated by :
+                # binder = binder.split(":")[0]
+
+                multimer_gap = 256
+
+                linker_sequence = "G" * 100 # need to make at least 100 so that it can circle all the way back and around
+
+                # they also suggest changing the order of the sequences
+                sequence = CD20_A + linker_sequence + CD20_B + linker_sequence + binder
+
+                multimer_gap = 256
+
+                residue_indices = list(range(1, len(sequence) + 1))
 
 
-        print('Running sequence')
+                residue_indices[len(CD20_A) + len(linker_sequence):] = [
+                    i + multimer_gap for i in residue_indices[len(CD20_A) + len(linker_sequence):]
+                ]
 
-        result = esmfold.fold(
-            sequence=sequence,
-            residue_indices=residue_indices,
-            print_pdb=True,
-            pdb_file=f"outputs/dimer5/EsmFold/fold_{i}.pdb",
-            device=device,
-        )
-        # save also result json, delete result.atoms
-        result_json = result.model_dump()
-        del result_json["atoms"]
-        with open(f"outputs/dimer5/EsmFold/fold_{i}.json", "w") as file:
-            json.dump(result_json, file)
-
-        print("Done")
+                residue_indices[len(CD20_A) + len(linker_sequence) + len(CD20_B) + len(linker_sequence):] = [
+                    i + multimer_gap for i in residue_indices[len(CD20_A) + len(linker_sequence) + len(CD20_B) + len(linker_sequence):]
+                ]
 
 
+                print('Running sequence')
+
+                if not os.path.exists(f"outputs/{design}/EsmFold/"):
+                    os.makedirs(f"outputs/{design}/EsmFold/")
+
+                result = esmfold.fold(
+                    sequence=sequence,
+                    residue_indices=residue_indices,
+                    print_pdb=True,
+                    pdb_file=f"outputs/{design}/EsmFold/fold_{j}_{i}.pdb",
+                    device=device,
+                )
+                # save also result json, delete result.atoms
+                # turn a dataclass into a dictionary
+                from dataclasses import asdict
+                import json
+                import numpy as np
+                result_dict = asdict(result)
+                del result_dict["atoms"]
+                # turn all attributes into strings (if it is a numpy array turn it into lists of lists)
+                for key, value in result_dict.items():
+                    if isinstance(value, (np.ndarray, torch.Tensor)):
+                        result_dict[key] = value.tolist()
+                    elif isinstance(value, dict):
+                        for k, v in value.items():
+                            if isinstance(v, (np.ndarray, torch.Tensor)):
+                                value[k] = v.tolist()
+                with open(f"outputs/{design}/EsmFold/fold_{j}_{i}.json", "w") as file:
+                    json.dump(result_dict, file)
+
+                print(f"Done {design} {j}")
 
 
